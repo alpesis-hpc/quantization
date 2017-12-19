787d19c2988a0b8eb2ed38d1fb808a8a4fb41bc8
K <kwailamchan@hotmail.com>
2017-12-19 12:56:22 +0800

    [master] initialized the project and implemented fix_bits. 	new file:   .gitignore 	new file:   README.rst 	new file:   quant/Makefile 	new file:   quant/quant.h 	new file:   quant/tests/test_quant.cc

---
+++ quant/quant.h
@@ -0,0 +1,40 @@
+/*
+ * Quantization
+ *
+ *    data           : 0.000000 0.699301 1.398601 2.097902 2.797203 3.496504 4.195804 4.895105 5.594406
+ *    data (quant)   : 0.000000 0.699219 1.398438 2.097656 2.796875 3.496094 4.195312 4.894531 5.593750
+ *    data (residual): 0.000000 0.000082 0.000163 0.000246 0.000328 0.000410 0.000492 0.000574 0.000656
+ */
+
+#ifndef QUANT_H
+#define QUANT_H
+
+#include <cmath>
+
+
+/*
+ */
+template <typename dtype>
+void fix_bits (dtype * data, const int n, unsigned int bitwidth, unsigned bitwidth_fraction)
+{
+	dtype intmax = pow(2, bitwidth - bitwidth_fraction - 1);
+	
+	for (int i = 0; i < n; ++i)
+	{
+		data[i] = (data[i] >= intmax) * intmax +
+				(data[i] <= -intmax) * (-intmax) +
+				(data[i] > -intmax && data[i] < intmax) *
+				round(data[i] * pow(2, bitwidth_fraction)) / pow(2, bitwidth_fraction);
+	}
+}
+
+
+template <typename dtype>
+void fix_dynamic (dtype * data, const int n)
+{
+	for (int i = 0; i < n; ++i)
+		data[i] = (int)ceil(log2(data[i]));
+}
+
+
+#endif
---
+++ quant/tests/test_quant.cc
@@ -0,0 +1,52 @@
+#include "../quant.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+template <typename dtype>
+void data_init(dtype * data, unsigned int n)
+{
+	for (int i = 0; i < n; ++i) data[i] = i * 24 / 1.43;
+}
+
+
+template <typename dtype>
+void data_print(dtype * data, unsigned int n)
+{
+	for (int i = 0; i < n; ++i) printf("%f ", data[i]);
+	printf("\n");
+}
+
+
+template <typename dtype>
+void test_fix_bits(dtype * data, unsigned int n)
+{
+	unsigned int bitwidth = 16;
+	unsigned int bitwidth_fraction = 7;
+	fix_bits(data, n, bitwidth, bitwidth_fraction);
+	printf("data (fixed: bits): "); data_print(data, n);
+}
+
+
+template <typename dtype>
+void test_fix_dynamic(dtype * data, unsigned int n)
+{
+	fix_dynamic(data, n);
+	printf("data (fixed: dynamic): "); data_print(data, n);
+}
+
+
+int main(void)
+{
+	unsigned int n = 10;
+	float * data = (float*)malloc(n*sizeof(float));
+	data_init(data, n); printf("data: "); data_print(data, n);
+
+	test_fix_bits(data, n);
+	test_fix_dynamic(data, n);
+
+	free(data);
+
+	return 0;
+}
